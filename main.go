0c6fa50c9a964d5d7544b7a66f5392f6523a86b3
ETairkhan <trouzerop@gmail.com>
2024-12-24 16:21:44 +0500

    palindrome

--- main.go
+++ main.go
@@ -6,11 +6,6 @@ import (
 )
 
 func main() {
-	var nums []int
-	nums = append(nums, 1, 1, 2)
-	fmt.Println(nums)
-
-	k := leetcodeProblems.RemoveDuplicates(nums)
-	fmt.Println(nums)
-	fmt.Println(k)
+	x := -121
+	fmt.Println(leetcodeProblems.IsPalindrome(x))
 }
