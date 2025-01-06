df2fb62efd2001e2b22ab6b2908f65d5e9a54b44
ETairkhan <trouzerop@gmail.com>
2025-01-06 17:57:30 +0500

    shuffle the array

--- leetcodeProblems/test.go
+++ leetcodeProblems/test.go
@@ -1,25 +1,15 @@
 package leetcodeProblems
 
-import (
-	"strconv"
-	"strings"
-)
 
-func ConvertDateToBinary(date string) string {
-	dict := strings.Split(date, "-") // Split the date into components
-	answer := ""
+func Shuffle(nums []int, n int) []int {
+	// Create a slice to store the result
+result := make([]int, 0, 2*n)
 
-	for i := 0; i < len(dict); i++ {
-		num, err := strconv.Atoi(dict[i]) // Convert string to integer
-		if err != nil {
-			return "Error: Invalid input" // Handle error if conversion fails
-		}
-		binary := strconv.FormatInt(int64(num), 2) // Convert integer to binary
-		if i > 0 {
-			answer += "-" // Add a separator between binary components
-		}
-		answer += binary
-	}
+// Iterate through the first half and second half of the array
+for i := 0; i < n; i++ {
+  result = append(result, nums[i])     // Append x_i
+  result = append(result, nums[i+n])  // Append y_i
+}
 
-	return answer
+return result
 }
--- main.go
+++ main.go
@@ -6,6 +6,19 @@ import (
 )
 
 func main() {
-	fmt.Println(leetcodeProblems.ConvertDateToBinary("2080-02-29"))
+	// Example 1
+	nums1 := []int{2, 5, 1, 3, 4, 7}
+	n1 := 3
+	fmt.Println(leetcodeProblems.Shuffle(nums1, n1)) // Output: [2, 3, 5, 4, 1, 7]
+
+	// Example 2
+	nums2 := []int{1, 2, 3, 4, 4, 3, 2, 1}
+	n2 := 4
+	fmt.Println(leetcodeProblems.Shuffle(nums2, n2)) // Output: [1, 4, 2, 3, 3, 2, 4, 1]
+
+	// Example 3
+	nums3 := []int{1, 1, 2, 2}
+	n3 := 2
+	fmt.Println(leetcodeProblems.Shuffle(nums3, n3)) // Output: [1, 2, 1, 2]
 	
 }
